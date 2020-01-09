9e621fd484462c15ae5cb940f162d5ea2ba27fae
Jas Spencer <jas.spencer414@gmail.com>
2020-01-09 16:44:51 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -7,7 +7,7 @@ class Artist < ActiveRecord::Base
   end
 
   def song_count
-  self.song.length  #return the number of songs associated with the artist
+  self.songs.length  #return the number of songs associated with the artist
   end
 
   def genre_count
