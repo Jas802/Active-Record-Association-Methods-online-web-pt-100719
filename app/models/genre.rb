0a464c99f126032dce1d3f4b6cccd574278648b1
Jas Spencer <jas.spencer414@gmail.com>
2020-01-09 16:45:34 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -11,6 +11,6 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-  self.arstists.map(&:name)  # return an array of strings containing every musician's name
+  self.artists.map(&:name)  # return an array of strings containing every musician's name
   end
 end
